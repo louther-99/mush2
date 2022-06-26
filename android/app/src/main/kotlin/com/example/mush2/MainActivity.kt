2d7b1eb5236043305f0b74d4cf1ca29fb3b7649e
louther-99 <79491632+louther-99@users.noreply.github.com>
2022-06-26 08:40:37 +0800

    removed firebase

--- android/app/src/main/kotlin/com/example/mush2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mush2/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.mush2
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+
 
 
 class MainActivity: FlutterActivity() {
