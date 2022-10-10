a03201878842de486e529e0a50e0cc985a2d0c78
louther-99 <79491632+louther-99@users.noreply.github.com>
2022-10-10 16:24:15 +0800

    added comment in the main

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ Future main() async {
   await Firebase.initializeApp(); //Initialize database
   //await AuthService().getOrCreateUser();
 
+//added comment in the main
 
   runApp(MyApp(
     // initialRoute: '/main',
